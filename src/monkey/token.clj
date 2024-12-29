202d89f477ae9f53550c6e430437ff9e622607b8
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-12-28 22:58:27 -0300

    feat(token): Introduce constant to represent tokens

---
+++ src/monkey/token.clj
@@ -0,0 +1,16 @@
+(ns monkey.token)
+
+(def TOKENS {:illegal "ILLEGAL"
+             :eof "EOF"
+             :ident "IDENT"
+             :int "INT"
+             :assign "="
+             :plus "+"
+             :comma ","
+             :semicolon ";"
+             :lparen "("
+             :rparen ")"
+             :lbrace "{"
+             :rbrace "}"
+             :function "FUNCTION"
+             :let "LET"})
